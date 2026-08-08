fab55a8bd7ba6921714b0b413f9edd9b2a768dd6
tedlundwall <tedlundwall@gmail.com>
2026-08-08 22:31:02 +0200

    handle more events

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ def main():
         site = requests.get(
             "https://www.thomann.se/ferrofish_pulse8_ae_b_stock.htm"
         )
-        if site.status_code != 404:
+        if site.status_code == 200:
             tkinter.messagebox.showwarning(
                 title=title, message="B-stock available!!", parent=root
             )
-        else:
+        elif site.status_code == 429:
+            tkinter.messagebox.showerror(
+                title=title, message="Check blocked by CAPTCHA.", parent=root
+            )
+        elif site.status_code == 404:
             tkinter.messagebox.showinfo(
                 title=title, message="No B-stock available.", parent=root
+            )
+        else:
+            tkinter.messagebox.showerror(
+                title=title, message="Unknown status code.", parent=root
             )
     except requests.ConnectionError:
         tkinter.messagebox.showerror(
